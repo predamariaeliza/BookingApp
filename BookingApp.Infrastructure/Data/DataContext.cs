a82e02d3fd7b005a12bc065681d5e87b2df3376e
maria <preda_maria_eliza@yahoo.com>
2024-04-22 20:37:29 +0300

    03 - added DataContext class for db

---
+++ BookingApp.Infrastructure/Data/DataContext.cs
@@ -0,0 +1,13 @@
+﻿using Microsoft.EntityFrameworkCore;
+
+namespace BookingApp.Infrastructure.Data
+{
+    public class DataContext : DbContext
+    {
+        public DataContext(DbContextOptions<DataContext> options) : base(options) 
+        { 
+            
+        }
+
+    }
+}
