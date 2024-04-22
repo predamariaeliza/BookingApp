450b6ba87ad0c1a45fa15c80be06a242d112b992
maria <preda_maria_eliza@yahoo.com>
2024-04-22 22:14:32 +0300

    01 - created controller for Property model

---
+++ BookingAppWeb/Controllers/PropertyController.cs
@@ -0,0 +1,22 @@
+﻿using BookingApp.Infrastructure.Data;
+using Microsoft.AspNetCore.Mvc;
+
+namespace BookingAppWeb.Controllers
+{
+
+    public class PropertyController : Controller
+    {
+        private readonly DataContext _dbContext;
+
+        public PropertyController(DataContext dbContext)
+        {
+            _dbContext = dbContext;
+        }
+
+        public IActionResult Index()
+        {
+            var properties = _dbContext.Properties.ToList();
+            return View();
+        }
+    }
+}
