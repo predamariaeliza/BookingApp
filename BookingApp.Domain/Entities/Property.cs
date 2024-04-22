d33683ae1f422931343968b4709b32ac030a6880
maria <preda_maria_eliza@yahoo.com>
2024-04-22 20:21:58 +0300

    02 - added Property model

---
+++ BookingApp.Domain/Entities/Property.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace BookingApp.Domain.Entities
+{
+    public class Property
+    {
+        public int Id { get; set; }
+        public required string Name { get; set; }
+        public string? Description { get; set; }
+        public double Price { get; set; }
+        public double SquareMeters { get; set; }
+        public double Occupancy { get; set; }
+        public string? ImageUrl { get; set; }
+        public DateTime? CreatedDate { get; set; }
+        public DateTime? UpdatedDate { get; set; }
+    }
+}
