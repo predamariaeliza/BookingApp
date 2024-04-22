b3ba5f9d80e3778f9bc1373c3c4ba557e1ea0c60
Maria Eliza <71140053+predamariaeliza@users.noreply.github.com>
2024-04-22 21:57:33 +0300

    01 - Project setup (#1) (#2)
    * 01 - added Application, Domain and Infrastructure class libraries projects
    
    * 02 - added Property model
    
    * 03 - added NuGet packages for EF Core
    
    * 03 - added DataContext class for db
    
    * 04 - added connection string
    
    * 05 - added DataContext in Program class
    
    * FIXED -  DefaultConnection in appsettings
    
    * 06 - added Properties table in db
    
    * 07 - seeded Properties values to db
    
    ---------
    
    Co-authored-by: maria <preda_maria_eliza@yahoo.com>

---
+++ BookingApp.Infrastructure/Data/DataContext.cs
@@ -0,0 +1,52 @@
+﻿using BookingApp.Domain.Entities;
+using Microsoft.EntityFrameworkCore;
+
+namespace BookingApp.Infrastructure.Data
+{
+    public class DataContext : DbContext
+    {
+        public DataContext(DbContextOptions<DataContext> options) : base(options) 
+        { 
+            
+        }
+
+        public DbSet<Property> Properties { get; set; }
+
+        protected override void OnModelCreating(ModelBuilder modelBuilder)
+        {
+            //base.OnModelCreating(modelBuilder);
+
+            modelBuilder.Entity<Property>().HasData(
+                new Property
+                {
+                     Id = 1,
+                     Name = "Premium Pool Villa",
+                     Description = "Description for Premium Pool Villa",
+                     ImageUrl = "https://placehold.co/600x401",
+                     Occupancy = 3,
+                     Price = 1000,
+                     SquareMeters = 100
+                },
+                new Property
+                {
+                    Id = 2,
+                    Name = "Austrian Villa",
+                    Description = "Description for Austrian Villa",
+                    ImageUrl = "https://placehold.co/600x402",
+                    Occupancy = 2,
+                    Price = 2000,
+                    SquareMeters = 200
+                },
+                new Property
+                {
+                    Id = 3,
+                    Name = "Italian Villa",
+                    Description = "Description for Italian Villa",
+                    ImageUrl = "https://placehold.co/600x403",
+                    Occupancy = 1,
+                    Price = 3000,
+                    SquareMeters = 300
+                });
+        }
+    }
+}
