1c1d9e17384bceca0e05aa48a74376f1fde5e648
Maria Eliza <71140053+predamariaeliza@users.noreply.github.com>
2024-04-23 22:19:21 +0300

    Property model (#3)
    * 01 - created controller for Property model
    
    * 02 - added Property Index navigation button
    
    * 03 - added list of Properties in UI
    
    * 04 - added button New Property in UI
    
    * 05 - dark bootstrap theme and bootstrap buttons
    
    * 06 - added View for Create Property
    
    * 07 - added Create cshtml page
    
    * 08 - added Create endpoint for Property
    
    * 09 - added validations for Create property UI
    
    * 10 - binding model error to Property
    
    ---------
    
    Co-authored-by: maria <preda_maria_eliza@yahoo.com>

---
+++ BookingAppWeb/Controllers/PropertyController.cs
@@ -0,0 +1,44 @@
+﻿using BookingApp.Domain.Entities;
+using BookingApp.Infrastructure.Data;
+using Microsoft.AspNetCore.Mvc;
+
+namespace BookingAppWeb.Controllers
+{
+
+    public class PropertyController : Controller
+    {
+        private readonly DataContext _dbContext;
+        public PropertyController(DataContext dbContext)
+        {
+            _dbContext = dbContext;
+        }
+
+        public IActionResult Index()
+        {
+            var properties = _dbContext.Properties.ToList();
+            return View(properties);
+        }
+
+        public IActionResult Create()
+        {
+            return View();
+        }
+
+        [HttpPost()]
+        public IActionResult Create(Property property)
+        {
+            if(property.Name == property.Description)
+            {
+                ModelState.AddModelError("name", "The Description cannot match the Name");
+            }
+
+            if(ModelState.IsValid)
+            {
+                _dbContext.Properties.Add(property);
+                _dbContext.SaveChanges();
+                return RedirectToAction("Index", "Property");
+            }
+            return View();
+        }
+    }
+}
