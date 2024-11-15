8f22eb2a0bd2bd8e385d744343f4951ddf69bb58
maria <preda_maria_eliza@yahoo.com>
2024-11-15 23:25:07 +0200

    01 - Added Property Repository Interface

---
+++ BookingApp.Application/Common/Interfaces/IPropertyRepository.cs
@@ -0,0 +1,15 @@
+﻿using BookingApp.Domain.Entities;
+using System.Linq.Expressions;
+
+namespace BookingApp.Application.Common.Interfaces
+{
+    public interface IPropertyRepository
+    {
+        IEnumerable<Property> GetAllProperties(Expression<Func<Property, bool>>? filter = null, string? includeProperties = null);
+        IEnumerable<Property> GetProperty(Expression<Func<Property, bool>> filter, string? includeProperties = null);
+        void CreateProperty(Property property);
+        void UpdateProperty(Property property);
+        void DeleteProperty(Property property);
+        void Save();
+    }
+}
