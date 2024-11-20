566f46d48879409f0a5394cdbe70df2162d57188
maria <preda_maria_eliza@yahoo.com>
2024-11-20 17:43:19 +0200

    06 - Implemented Generic Repository

---
+++ BookingApp.Application/Common/Interfaces/IRepository.cs
@@ -0,0 +1,12 @@
+﻿using System.Linq.Expressions;
+
+namespace BookingApp.Application.Common.Interfaces
+{
+    public interface IRepository<T> where T : class
+    {
+        IEnumerable<T> GetAll(Expression<Func<T, bool>>? filter = null, string? includeProperties = null);
+        T Get(Expression<Func<T, bool>> filter, string? includeProperties = null);
+        void Create(T property);
+        void Delete(T property);
+    }
+}
---
+++ BookingApp.Infrastructure/Repository/Repository.cs
@@ -0,0 +1,28 @@
+﻿using BookingApp.Application.Common.Interfaces;
+using System.Linq.Expressions;
+
+namespace BookingApp.Infrastructure.Repository
+{
+    public class Repository<T> : IRepository<T> where T : class
+    {
+        public void Create(T property)
+        {
+            throw new NotImplementedException();
+        }
+
+        public T Get(Expression<Func<T, bool>> filter, string? includeProperties = null)
+        {
+            throw new NotImplementedException();
+        }
+
+        public IEnumerable<T> GetAll(Expression<Func<T, bool>>? filter = null, string? includeProperties = null)
+        {
+            throw new NotImplementedException();
+        }
+
+        public void Delete(T property)
+        {
+            throw new NotImplementedException();
+        }
+    }
+}
