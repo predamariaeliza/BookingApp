80f8e765b8b3491e228de3ec040a4e64e9a1ea1b
maria <preda_maria_eliza@yahoo.com>
2025-01-12 21:07:28 +0200

    06 - added Account Controller

---
+++ BookingAppWeb/Controllers/AccountController.cs
@@ -0,0 +1,32 @@
+﻿using BookingApp.Application.Common.Interfaces;
+using BookingApp.Domain.Entities;
+using Microsoft.AspNetCore.Identity;
+using Microsoft.AspNetCore.Mvc;
+
+namespace BookingAppWeb.Controllers
+{
+    public class AccountController : Controller
+    {
+        private readonly IUnitOfWork _unitOfWork;
+        private readonly UserManager<ApplicationUser> _userManager;
+        private readonly SignInManager<ApplicationUser> _signInManager;
+        private RoleManager<IdentityRole> _roleManager;
+
+        public AccountController(IUnitOfWork unitOfWork, UserManager<ApplicationUser> userManager, SignInManager<ApplicationUser> signInManager, RoleManager<IdentityRole> roleManager)
+        {
+            _unitOfWork = unitOfWork;
+            _userManager = userManager;
+            _signInManager = signInManager;
+            _roleManager = roleManager;
+        }
+
+        public IActionResult Login()
+        {
+            return View();
+        }
+        public IActionResult Register ()
+        {
+            return View();
+        }
+    }
+}
