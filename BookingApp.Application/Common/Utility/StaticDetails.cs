01bf7a84742109dea5d5bcaba36c646b9da8d2ab
Maria Eliza <71140053+predamariaeliza@users.noreply.github.com>
2025-01-13 23:24:09 +0200

    11 - .NET Identity (2) (#13)
    * 09 - Added User Roles in the AspNetRoles table (using C# code)
    
    * 10 - Retrived Roles in Register Dropdown (UI)
    
    * 11 - Added StaticDetails public static class
    
    * 12 - POST endpoint for Register in AccountController
    
    * 13 - POST endpoint for Login in AccountController

---
+++ BookingApp.Application/Common/Utility/StaticDetails.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace BookingApp.Application.Common.Utility
+{
+    public static class StaticDetails
+    {
+        public const string Role_Admin = "Admin";
+        public const string Role_Customer = "Customer";
+        public const string Role_PropertyOwner = "PropertyOwner";
+    }
+}
--- BookingAppWeb/Controllers/AccountController.cs
+++ BookingAppWeb/Controllers/AccountController.cs
@@ -1,8 +1,10 @@
 ﻿using BookingApp.Application.Common.Interfaces;
+using BookingApp.Application.Common.Utility;
 using BookingApp.Domain.Entities;
 using BookingAppWeb.ViewModels;
 using Microsoft.AspNetCore.Identity;
 using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.Mvc.Rendering;
 
 namespace BookingAppWeb.Controllers
 {
@@ -34,9 +36,136 @@ namespace BookingAppWeb.Controllers
 
             return View(loginVM);
         }
+
         public IActionResult Register ()
         {
-            return View();
+            if(!_roleManager.RoleExistsAsync(StaticDetails.Role_Admin).GetAwaiter().GetResult())
+            {
+                // .Wait() asteapta ca task-ul sa fie complet executat
+                // .Wait() => inlocuitor pentru async & await
+                _roleManager.CreateAsync(new IdentityRole(StaticDetails.Role_Admin)).Wait();
+                _roleManager.CreateAsync(new IdentityRole(StaticDetails.Role_Customer)).Wait();
+                _roleManager.CreateAsync(new IdentityRole(StaticDetails.Role_PropertyOwner)).Wait(); 
+            }
+
+            RegisterVM registerVM = new()
+            {
+                RoleList = _roleManager.Roles.Select(x => new SelectListItem
+                {
+                    Text = x.Name,
+                    Value = x.Name
+                })
+            };
+
+            return View(registerVM);
+        }
+
+        [HttpPost]
+        public async Task<IActionResult> Register(RegisterVM registerVM)
+        {
+            ApplicationUser user = new()
+            {
+                Name = registerVM.Name,
+                Email = registerVM.Email,
+                PhoneNumber = registerVM.PhoneNumber,
+                NormalizedEmail = registerVM.Email.ToUpper(),
+                EmailConfirmed = true,
+                UserName = registerVM.Email,
+                CreatedAt = DateTime.Now,
+            };
+
+            var result = await _userManager.CreateAsync(user, registerVM.Password);
+
+            // daca rezultatul este un succes
+            if (result.Succeeded)
+            {
+                // ADAUGARE/ASIGNARE ROL USER
+                // daca exista cel putin un rol selectat
+                if (!string.IsNullOrEmpty(registerVM.Role))
+                {
+                    // se adauga un singur rol -> AddToRolesAsync pt mai multe roluri
+                    await _userManager.AddToRoleAsync(user, registerVM.Role);
+                }
+                // daca nu exista niciun rol selectat, userul primeste automat rolul de Customer
+                else
+                {
+                    await _userManager.AddToRoleAsync(user, StaticDetails.Role_Customer);
+                }
+
+                // isPersistent:false - sign in the user in mod automat
+                await _signInManager.SignInAsync(user, isPersistent: false);
+
+                // redirectionam catre:
+                if (string.IsNullOrEmpty(registerVM.RedirectURL))
+                {
+                    // pagina de Home - daca nu exista redirectURL
+                    return RedirectToAction("Index", "Home");
+                }
+                else
+                {
+                    // Url - ul Redirectionat - daca exista redirectURL
+                    // LocalRedirect -> redirectionam mereu catre domeniul local
+                    // * nu punem direct link-ul pentru a nu redirectiona catre site-uri malitioase
+                    // * (security measure)
+                    return LocalRedirect(registerVM.RedirectURL);
+                }
+
+            }
+
+            // daca rezultatul nu este un succes
+            // afisam eroarea
+            foreach (var error in result.Errors)
+            {
+                // nu adaugam KEY, se va afisa eroarea de pe UI (asp-validation-summary = "ModelOnly" class = "text-danger")
+                ModelState.AddModelError("", error.Description);
+            }
+
+            // populam lista row
+            registerVM.RoleList = _roleManager.Roles.Select(x => new SelectListItem
+            {
+                Text = x.Name,
+                Value = x.Name
+            });
+        
+            return View(registerVM);
+        }
+
+        [HttpPost]
+        public async Task<IActionResult> Login(LoginVM loginVM)
+        {
+            if(ModelState.IsValid)
+            {
+                // verificare User & Parola
+                var result = await _signInManager
+                    .PasswordSignInAsync(loginVM.Email, loginVM.Password, loginVM.RememberMe, lockoutOnFailure:false);
+
+                // daca rezultatul este un succes
+                if (result.Succeeded)
+                {
+                    // redirectionam catre:
+                    if (string.IsNullOrEmpty(loginVM.RedirectURL))
+                    {
+                        // pagina de Home - daca nu exista redirectURL
+                        return RedirectToAction("Index", "Home");
+                    }
+                    else
+                    {
+                        // Url - ul Redirectionat - daca exista redirectURL
+                        // LocalRedirect -> redirectionam mereu catre domeniul local
+                        // * nu punem direct link-ul pentru a nu redirectiona catre site-uri malitioase
+                        // * (security measure)
+                        return LocalRedirect(loginVM.RedirectURL);
+                    }
+                }
+                // daca rezultatul nu este un succes
+                // => nu vrem sa aratam utilizatorului eroarea
+                else
+                {
+                    ModelState.AddModelError("", "Invalid login attempt.");
+                }
+            }
+
+            return View(loginVM);
         }
     }
 }
