017520bd09373e6e53ad122dedad99789d7eb2e6
predamariaeliza <predamariaeliza06@gmail.com>
2025-01-14 20:16:32 +0200

    17 - Access Denied (UI) page

--- BookingAppWeb/Controllers/AccountController.cs
+++ BookingAppWeb/Controllers/AccountController.cs
@@ -42,6 +42,11 @@ namespace BookingAppWeb.Controllers
             return RedirectToAction("Index","Home");
         }
 
+        public IActionResult AccessDenied()
+        {
+            return View();
+        }
+
         public IActionResult Register ()
         {
             if(!_roleManager.RoleExistsAsync(StaticDetails.Role_Admin).GetAwaiter().GetResult())
