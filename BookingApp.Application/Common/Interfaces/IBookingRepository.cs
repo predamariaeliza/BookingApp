6c299137798a4f002bd85d63d58f0b155a1d9284
predamariaeliza <predamariaeliza06@gmail.com>
2025-02-12 17:32:08 +0200

    02 - Created Booking Table in DB & Added Booking Repository

---
+++ BookingApp.Application/Common/Interfaces/IBookingRepository.cs
@@ -0,0 +1,9 @@
+﻿using BookingApp.Domain.Entities;
+
+namespace BookingApp.Application.Common.Interfaces
+{
+    public interface IBookingRepository : IRepository<Booking>
+    {
+        void Update(Booking entity);
+    }
+}
---
+++ BookingApp.Infrastructure/Repository/BookingRepository.cs
@@ -0,0 +1,20 @@
+﻿using BookingApp.Application.Common.Interfaces;
+using BookingApp.Domain.Entities;
+using BookingApp.Infrastructure.Data;
+
+namespace BookingApp.Infrastructure.Repository
+{
+    public class BookingRepository : Repository<Booking>, IBookingRepository
+    {
+        private readonly DataContext _dbContext;
+        public BookingRepository(DataContext dbContext) : base(dbContext)
+        {
+            _dbContext = dbContext;
+        }
+        
+        public void Update(Booking entity)
+        {
+            _dbContext.Bookings.Update(entity);
+        }
+    }
+}
