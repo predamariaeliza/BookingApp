b1918eea4c12a9b71f3eaa43a676ae7da6482f32
predamariaeliza <predamariaeliza06@gmail.com>
2025-03-15 10:53:57 +0200

    12 - created Update Status & Update Stripe Payment Id methods in Booking repository

--- BookingApp.Infrastructure/Repository/BookingRepository.cs
+++ BookingApp.Infrastructure/Repository/BookingRepository.cs
@@ -1,4 +1,5 @@
 ﻿using BookingApp.Application.Common.Interfaces;
+using BookingApp.Application.Common.Utility;
 using BookingApp.Domain.Entities;
 using BookingApp.Infrastructure.Data;
 
@@ -15,6 +16,46 @@ namespace BookingApp.Infrastructure.Repository
         public void Update(Booking entity)
         {
             _dbContext.Bookings.Update(entity);
+        }
+
+        public void UpdateStatus(int bookingId, string bookingStatus)
+        {
+            var booking = _dbContext.Bookings.FirstOrDefault(b => b.Id == bookingId);
+            if(booking != null)
+            {
+                booking.Status = bookingStatus;
+
+                //if the user is checked in
+                if(bookingStatus == StaticDetails.StatusCheckedIn)
+                {
+                    booking.ActualCheckInDate = DateTime.Now;
+                }
+
+                //if the user has checked out
+                if (bookingStatus == StaticDetails.StatusCompleted)
+                {
+                    booking.ActualCheckInDate = DateTime.Now;
+                }
+                _dbContext.SaveChanges();
+            }
+        }
+
+        public void UpdateStripePaymentId(int bookingId, string sessionId, string paymentIntendId)
+        {
+            var booking = _dbContext.Bookings.FirstOrDefault(b => b.Id == bookingId);
+            if (booking != null)
+            {
+                if(!string.IsNullOrEmpty(sessionId))
+                {
+                    booking.StripeSessionId = sessionId;
+                }
+                if (!string.IsNullOrEmpty(sessionId))
+                {
+                    booking.StripePaymentIntentId = paymentIntendId;
+                    booking.PaymentDate = DateTime.Now;
+                    booking.IsPaymentSuccessful = true;
+                }
+            }
         }
     }
 }
