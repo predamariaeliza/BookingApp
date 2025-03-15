4521e4314853ca317d14da0c61aedb633fbd2427
Maria Eliza <71140053+predamariaeliza@users.noreply.github.com>
2025-03-15 18:46:15 +0200

    13 - Booking (4) (#22)
    * 12 - created Update Status & Update Stripe Payment Id methods in Booking repository
    
    * 15 - Added Stripe Keys & Nuget Package
    
    * 16 - Created session in Stripe
    
    * 17 - Stripe in Action
    
    * 18 Stripe Payment Intend Id

--- BookingApp.Application/Common/Interfaces/IBookingRepository.cs
+++ BookingApp.Application/Common/Interfaces/IBookingRepository.cs
@@ -5,5 +5,7 @@ namespace BookingApp.Application.Common.Interfaces
     public interface IBookingRepository : IRepository<Booking>
     {
         void Update(Booking entity);
+        void UpdateStatus(int bookingId, string bookingStatus);
+        void UpdateStripePaymentId(int bookingId, string sessionId, string paymentIntendId);
     }
 }
--- BookingAppWeb/Controllers/BookingController.cs
+++ BookingAppWeb/Controllers/BookingController.cs
@@ -3,6 +3,8 @@ using BookingApp.Application.Common.Utility;
 using BookingApp.Domain.Entities;
 using Microsoft.AspNetCore.Authorization;
 using Microsoft.AspNetCore.Mvc;
+using Stripe;
+using Stripe.Checkout;
 using System.Runtime.InteropServices;
 using System.Security.Claims;
 
@@ -58,14 +60,70 @@ namespace BookingAppWeb.Controllers
 
             _unitOfWork.Booking.Create(booking);
             _unitOfWork.Save();
-
-            return RedirectToAction(nameof(BookingConfirmation), new { bookingId = booking.Id });
+            return StripeSession(booking, property);
         }
 
         [Authorize]
         public IActionResult BookingConfirmation(int bookingId)
         {
+            Booking booking = _unitOfWork.Booking.Get(b => b.Id == bookingId, includeProperties: "Property");
+
+            if (booking.Status == StaticDetails.StatusPending)
+            {
+                // this is a pending order, we need to confirm if the payment was successful
+                var service = new SessionService();
+                Session session = service.Get(booking.StripeSessionId);
+
+                //if the payment was successful, we update the booking status
+                if (session.PaymentStatus == "paid")
+                {
+                    _unitOfWork.Booking.UpdateStatus(booking.Id, StaticDetails.StatusApproved);
+                    _unitOfWork.Booking.UpdateStripePaymentId(booking.Id, session.Id, session.PaymentIntentId);
+                    _unitOfWork.Save();
+                }
+            }
             return View(bookingId);
         }
+
+
+        #region private
+        private StatusCodeResult StripeSession(Booking booking, Property property)
+        {
+            var domain = Request.Scheme + "://" + Request.Host.Value; //adresa localhost
+            var options = new SessionCreateOptions
+            {
+                LineItems = new List<SessionLineItemOptions>(),
+                Mode = "payment",
+                SuccessUrl = domain + $"/booking/BookingConfirmation?bookingId={booking.Id}", //redirect-ul platii cu succes
+                CancelUrl = domain + $"/booking/FinalizeBooking?propertyId={booking.PropertyId}&checkInDate={booking.CheckInDate}&nights={booking.Nights}", //redirect-ul platii cancelate
+            };
+
+            options.LineItems.Add(new SessionLineItemOptions
+            {
+                PriceData = new SessionLineItemPriceDataOptions
+                {
+                    UnitAmount = (long)(booking.TotalCost * 100),
+                    Currency = "ron",
+                    ProductData = new SessionLineItemPriceDataProductDataOptions
+                    {
+                        Name = property.Name,
+                        //Description = property.Description,
+                        //Images = new List<string> { domain + property.ImageUrl
+                    },
+                },
+                Quantity = 1,
+            });
+
+            var service = new SessionService();
+            Session session = service.Create(options);
+
+            _unitOfWork.Booking.UpdateStripePaymentId(booking.Id, session.Id, session.PaymentIntentId);
+            _unitOfWork.Save();
+
+            Response.Headers.Add("Location", session.Url);
+            return new StatusCodeResult(303);
+        }
+
+        #endregion
     }
 }
