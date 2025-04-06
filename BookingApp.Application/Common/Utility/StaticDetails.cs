138c29a539efcb5dff983225d1fde26b9e12390c
predamariaeliza <predamariaeliza06@gmail.com>
2025-04-06 11:47:24 +0300

    11 - Calculate Booking Availability on StaticDetails

--- BookingApp.Application/Common/Utility/StaticDetails.cs
+++ BookingApp.Application/Common/Utility/StaticDetails.cs
@@ -1,6 +1,8 @@
-﻿using System;
+﻿using BookingApp.Domain.Entities;
+using System;
 using System.Collections.Generic;
 using System.Linq;
+using System.Security.Principal;
 using System.Text;
 using System.Threading.Tasks;
 
@@ -18,5 +20,35 @@ namespace BookingApp.Application.Common.Utility
         public const string StatusCompleted = "Completed";
         public const string StatusCancelled = "Cancelled";
         public const string StatusRefunded = "Refunded";
+
+        public static int PropertyRoomsAvailable_Count(int propertyId, List<PropertyNumber> propertyNumberList, DateOnly checkInDate, int nights, List<Booking> bookings)
+        {
+            //how many bookings collide with our date
+            List<int> bookingInDate = new();
+
+            var roomsInProperty = propertyNumberList.Where(x => x.PropertyId == propertyId).Count();
+            for(int i = 0; i < nights; i++)
+            {
+                //if property is booked for the selected criterias
+                var propertyBooked = bookings.Where(u => u.CheckInDate <= checkInDate.AddDays(i)
+                && u.CheckOutDate > checkInDate.AddDays(i) 
+                && u.PropertyId == propertyId);
+
+                foreach(var booking in propertyBooked)
+                {
+                    if(!bookingInDate.Contains(booking.Id))
+                    {
+                        bookingInDate.Add(booking.Id);
+                    }
+                }
+
+                //calculate on that particular night how many rooms are available
+                var totalAvailableRooms = roomsInProperty - bookingInDate.Count;
+                if(totalAvailableRooms == 0)
+                {
+                    return 0;
+                }
+            }
+        }
     }
 }
