7e93223dcb5f928eb6991281a31e54a847bbeafc
predamariaeliza <predamariaeliza06@gmail.com>
2025-04-06 11:54:37 +0300

    12 - Booking Availability Part 2, this logic needs more focus !!!

--- BookingApp.Application/Common/Utility/StaticDetails.cs
+++ BookingApp.Application/Common/Utility/StaticDetails.cs
@@ -21,13 +21,25 @@ namespace BookingApp.Application.Common.Utility
         public const string StatusCancelled = "Cancelled";
         public const string StatusRefunded = "Refunded";
 
+        /// <summary>
+        /// This is a function that is capable of finding the property rooms that are available, based on the parameter that we pass in our method
+        /// This logic might break, you need to focus more on this !!!
+        /// </summary>
+        /// <param name="propertyId"></param>
+        /// <param name="propertyNumberList"></param>
+        /// <param name="checkInDate"></param>
+        /// <param name="nights"></param>
+        /// <param name="bookings"></param>
+        /// <returns></returns>
         public static int PropertyRoomsAvailable_Count(int propertyId, List<PropertyNumber> propertyNumberList, DateOnly checkInDate, int nights, List<Booking> bookings)
         {
             //how many bookings collide with our date
             List<int> bookingInDate = new();
-
+            int finalAvailableRoomForAllNights = int.MaxValue;
             var roomsInProperty = propertyNumberList.Where(x => x.PropertyId == propertyId).Count();
-            for(int i = 0; i < nights; i++)
+
+            //this is the total number of rooms in the property available for each night
+            for (int i = 0; i < nights; i++)
             {
                 //if property is booked for the selected criterias
                 var propertyBooked = bookings.Where(u => u.CheckInDate <= checkInDate.AddDays(i)
@@ -44,11 +56,21 @@ namespace BookingApp.Application.Common.Utility
 
                 //calculate on that particular night how many rooms are available
                 var totalAvailableRooms = roomsInProperty - bookingInDate.Count;
-                if(totalAvailableRooms == 0)
+                if (totalAvailableRooms == 0)
                 {
                     return 0;
                 }
+                else
+                {
+                    //how we get the lowest value of the available rooms for every night
+                    if (finalAvailableRoomForAllNights > totalAvailableRooms)
+                    {
+                        finalAvailableRoomForAllNights = totalAvailableRooms;
+                    }
+                }
             }
+
+            return finalAvailableRoomForAllNights;
         }
     }
 }
