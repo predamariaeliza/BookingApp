4da85a14f2a487fbc488aac80331464e42a33b2e
predamariaeliza <predamariaeliza06@gmail.com>
2025-04-07 20:45:03 +0300

    04 - Added Endpoint for Loading Bookings Data in DashboardController

--- BookingAppWeb/Controllers/DashboardController.cs
+++ BookingAppWeb/Controllers/DashboardController.cs
@@ -1,12 +1,37 @@
-﻿using Microsoft.AspNetCore.Mvc;
+﻿using BookingApp.Application.Common.Interfaces;
+using BookingApp.Application.Common.Utility;
+using Microsoft.AspNetCore.Mvc;
 
 namespace BookingAppWeb.Controllers
 {
     public class DashboardController : Controller
     {
+        private readonly IUnitOfWork _unitOfWork;
+        static int previousMonth = DateTime.Now.Month == 1? 12: DateTime.Now.Month - 1;
+        readonly DateTime previousMonthStartDate = new(DateTime.Now.Year, previousMonth, 1);
+        readonly DateTime currentMonthStartDate = new(DateTime.Now.Year, DateTime.Now.Month - 1, 1);
+
+        public DashboardController(IUnitOfWork unitOfWork)
+        {
+            _unitOfWork = unitOfWork;
+        }
         public IActionResult Index()
         {
             return View();
+        }
+
+        public async Task<IActionResult> GetTotalBookingRadialChartData()
+        {
+            var totalBookings = _unitOfWork.Booking.GetAll(u => u.Status != StaticDetails.StatusPending
+            || u.Status == StaticDetails.StatusCancelled);
+
+            var countByCurrentMonth = totalBookings.Count(u => u.BookingDate >= currentMonthStartDate
+            && u.BookingDate <= DateTime.Now);
+
+            var countByPreviousMonth = totalBookings.Count(u => u.BookingDate >= previousMonthStartDate
+            && u.BookingDate <= currentMonthStartDate);
+
+
         }
     }
 }
