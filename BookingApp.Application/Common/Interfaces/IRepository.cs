fcf8f1344b0fd4417f44657a87f90ab65224cffc
predamariaeliza <predamariaeliza06@gmail.com>
2025-04-19 13:21:52 +0300

    01 - as no tracking EF Core

--- BookingApp.Application/Common/Interfaces/IRepository.cs
+++ BookingApp.Application/Common/Interfaces/IRepository.cs
@@ -4,8 +4,8 @@ namespace BookingApp.Application.Common.Interfaces
 {
     public interface IRepository<T> where T : class
     {
-        IEnumerable<T> GetAll(Expression<Func<T, bool>>? filter = null, string? includeProperties = null);
-        T Get(Expression<Func<T, bool>> filter, string? includeProperties = null);
+        IEnumerable<T> GetAll(Expression<Func<T, bool>>? filter = null, string? includeProperties = null, bool tracked = false);
+        T Get(Expression<Func<T, bool>> filter, string? includeProperties = null, bool tracked = false);
         void Create(T property);
         bool Any(Expression<Func<T, bool>>? filter);
         void Delete(T property);
